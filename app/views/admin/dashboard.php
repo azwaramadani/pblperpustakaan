047ff9bcc0b7766b6a73febea3c4f451bd8c56e4
Thierry <thierryud2476@gmail.com>
2025-11-10 13:03:52 +0700

    ada ubahan di views/admin

--- app/views/admin/dashboard.php
+++ app/views/admin/dashboard.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Rudy Ruang Study</title>
-  <link rel="stylesheet" href="../../../public/assets/css/style2.css">
+  <link rel="stylesheet" href="../../../public/assets/css/styleh2.css">
 </head>
 
 <body>
